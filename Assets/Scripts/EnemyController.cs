3e887db916d47f343d9e4fc4a12eba212fa0842b
ishi 3 <ishiliagilcedeslabrador@gmail.com>
2025-10-01 11:20:41 -0400

    Added a audio source to the Enemy Game Object
    Edited the  EnemyController script with hit reaction: plays animation, particles, sound, and updates score

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -3,17 +3,25 @@ using UnityEngine;
 public class EnemyController : MonoBehaviour
 {
     private Animator animator;
-    private ParticleSystem system;
+    private ParticleSystem hitParticles;
+    private AudioSource audioSource;
+
     void Start()
     {
         animator = GetComponent<Animator>();
-        system = GetComponent<ParticleSystem>();
+        hitParticles = GetComponent<ParticleSystem>();
+        audioSource = GetComponent<AudioSource>();
     }
 
     public void GotHit()
     {
-        animator.ResetTrigger("GotHit");
         animator.SetTrigger("GotHit");
+        hitParticles.Play();
+        audioSource.Play();
 
+        if (GameManager.Instance != null)
+        {
+            GameManager.Instance.AddScore(1); // Add 1 point per hit
+        }
     }
 }
